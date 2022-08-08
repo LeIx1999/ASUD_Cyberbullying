881bfe26550a246878dfcda034238abd14de0944
Jannis Niesen <Jannis.nie99@gmail.com>
2022-08-08 22:11:37 +0200

    Docker ready

--- Code/Flask_API/app.py
+++ Code/Flask_API/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request, render_template
 from sentence_transformers import SentenceTransformer
 from joblib import load
-from sklearn.ensemble import RandomForestClassifier
+
 app = Flask(__name__)
 
 @app.route('/')
